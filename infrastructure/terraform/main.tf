b14d39832ab6c1de9f26e960a767cc0e975f817f
david-iaggbs <david.parra@iaggbs.com>
2019-06-10 13:21:39 +0200

    enable use of modules

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -3,8 +3,6 @@
 ##################################################################################
 
 provider "aws" {
-  # access_key = "${var.aws_access_key}"
-  # secret_key = "${var.aws_secret_key}"
   region     = "us-west-1"
 }
 
@@ -13,180 +11,41 @@ provider "aws" {
 ##################################################################################
 
 locals {
-  #action = "${lookup(var.action_on_environments, terraform.workspace, "deployment")}"
-  #action = "${lookup(var.action_on_environments, var.action, "deployment")}"
   test_size = "${var.action == "test" ? 1 : 0}"
-  #test_size = "${local.functional_test == "test" ? 1 : 0}"
-  # size = "${local.environment == "dev" ? lookup(var.workspace_to_size_map, terraform.workspace, "small") : var.environment_to_size_map[local.environment]}"
+}
+
+##################################################################################
+# MODULES
+##################################################################################
+
+module "vpc" {
+  #source = "github.com/terraform-community-modules/tf_aws_vpc?ref=v1.0.0"
+  source = "./modules/vpc"
+
+  aws_access_key  = "${var.aws_access_key}"
+  aws_secret_key  = "${var.aws_secret_key}"
+}
+
+module "ec2" {
+  #source = "github.com/terraform-community-modules/tf_aws_vpc?ref=v1.0.0"
+  source = "./modules/ec2"
+
+  aws_access_key    = "${var.aws_access_key}"
+  aws_secret_key    = "${var.aws_secret_key}"
+  AMI               = "${var.AMI}"
+  key_name          = "${var.key_name}"
+  private_key_path  = "${var.private_key_path}"
+  environment       = "${var.environment}"
+  stackname         = "${var.stackname}"
+  subnet1_id        = "${module.vpc.subnet1_id}"
+  subnet2_id        = "${module.vpc.subnet2_id}"
+  nginx-sg_id       = "${module.vpc.nginx-sg_id}"
 }
 
 ##################################################################################
 # DATA
 ##################################################################################
 
-data "aws_availability_zones" "available" {}
-
 ##################################################################################
 # RESOURCES
 ##################################################################################
-
-# NETWORKING #
-resource "aws_vpc" "vpc" {
-  cidr_block = "${var.network_address_space}"
-  enable_dns_hostnames = "true"
-
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
-
-}
-
-resource "aws_subnet" "subnet1" {
-  cidr_block        = "${var.subnet1_address_space}"
-  vpc_id            = "${aws_vpc.vpc.id}"
-  map_public_ip_on_launch = "true"
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
-
-}
-
-resource "aws_subnet" "subnet2" {
-  cidr_block        = "${var.subnet2_address_space}"
-  vpc_id            = "${aws_vpc.vpc.id}"
-  map_public_ip_on_launch = "true"
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"
-
-}
-
-# ROUTING #
-resource "aws_route_table" "rtb" {
-  vpc_id = "${aws_vpc.vpc.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.igw.id}"
-  }
-}
-
-resource "aws_route_table_association" "rta-subnet1" {
-  subnet_id      = "${aws_subnet.subnet1.id}"
-  route_table_id = "${aws_route_table.rtb.id}"
-}
-
-resource "aws_route_table_association" "rta-subnet2" {
-  subnet_id      = "${aws_subnet.subnet2.id}"
-  route_table_id = "${aws_route_table.rtb.id}"
-}
-
-# SECURITY GROUPS #
-# Nginx security group 
-resource "aws_security_group" "nginx-sg" {
-  name        = "nginx_sg"
-  vpc_id      = "${aws_vpc.vpc.id}"
-
-  # SSH access from anywhere
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTP access from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTP access from anywhere
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTP access from anywhere
-  ingress {
-    from_port   = 4444
-    to_port     = 4444
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTP access from anywhere
-  ingress {
-    from_port   = 5555
-    to_port     = 5555
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # outbound internet access
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# INSTANCES #
-resource "aws_instance" "coreinstance" {
-  ami           = "${var.AMI}"
-  instance_type = "t2.micro"
-  subnet_id     = "${aws_subnet.subnet1.id}"
-  vpc_security_group_ids = ["${aws_security_group.nginx-sg.id}"]
-  key_name        = "${var.key_name}"
-
-  connection {
-    user        = "ec2-user"
-    private_key = "${file(var.private_key_path)}"
-  }
-
-  tags = {
-    Role = "Core Instance"
-    environment = "${var.environment}"
-    stackname = "${var.stackname}"
-    logicalid = "EC2Instance"
-  }
-}
-
-resource "aws_instance" "testinstance" {
-  count = "${local.test_size}"
-  ami           = "ami-074e2d6769f445be5"
-  instance_type = "t2.micro"
-  subnet_id     = "${aws_subnet.subnet1.id}"
-  vpc_security_group_ids = ["${aws_security_group.nginx-sg.id}"]
-  key_name        = "${var.key_name}"
-
-  connection {
-    user        = "ec2-user"
-    private_key = "${file(var.private_key_path)}"
-  }
-
-  tags = {
-    Role = "Test Instance"
-    environment = "${var.environment}"
-    stackname = "${var.stackname}"
-    logicalid = "EC2Instance"
-  }
-}
-
-# ##################################################################################
-# # OUTPUT
-# ##################################################################################
-
-# output "InstanceId" {
-#     value = "${aws_instance.coreinstance.id}"
-# }
-
-# output "PublicDNS" {
-#     value = "${aws_instance.coreinstance.public_dns}"
-# }
-
-# output "PublicIP" {
-#     value = "${aws_instance.coreinstance.public_ip}"
-# }
---
+++ infrastructure/terraform/modules/vpc/main.tf
@@ -0,0 +1,128 @@
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  region     = "us-west-1"
+}
+
+##################################################################################
+# LOCALS
+##################################################################################
+
+##################################################################################
+# MODULES
+##################################################################################
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_availability_zones" "available" {}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+# NETWORKING #
+resource "aws_vpc" "vpc" {
+  cidr_block = "${var.network_address_space}"
+  enable_dns_hostnames = "true"
+
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block        = "${var.subnet1_address_space}"
+  vpc_id            = "${aws_vpc.vpc.id}"
+  map_public_ip_on_launch = "true"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block        = "${var.subnet2_address_space}"
+  vpc_id            = "${aws_vpc.vpc.id}"
+  map_public_ip_on_launch = "true"
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+
+}
+
+# ROUTING #
+resource "aws_route_table" "rtb" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+}
+
+resource "aws_route_table_association" "rta-subnet1" {
+  subnet_id      = "${aws_subnet.subnet1.id}"
+  route_table_id = "${aws_route_table.rtb.id}"
+}
+
+resource "aws_route_table_association" "rta-subnet2" {
+  subnet_id      = "${aws_subnet.subnet2.id}"
+  route_table_id = "${aws_route_table.rtb.id}"
+}
+
+# SECURITY GROUPS #
+# Nginx security group 
+resource "aws_security_group" "nginx-sg" {
+  name        = "nginx_sg"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 4444
+    to_port     = 4444
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 5555
+    to_port     = 5555
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
